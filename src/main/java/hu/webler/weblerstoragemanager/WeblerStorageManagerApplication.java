eca36eff2ae02e58ddcce1ef9de32aa66a51f60c
Csaba Vadász <csabavadasz79@gmail.com>
2024-02-16 21:30:14 +0100

    Initial commit

---
+++ src/main/java/hu/webler/weblerstoragemanager/WeblerStorageManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.webler.weblerstoragemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblerStorageManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblerStorageManagerApplication.class, args);
+    }
+
+}
