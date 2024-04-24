7e0ed5c227fbe7e98751c146bc86756308d0d2b3
Csaba Vadász <csabavadasz79@gmail.com>
2024-04-24 19:52:39 +0200

    add @Autowired annotation

--- src/test/java/hu/webler/weblerstoragemanager/controller/ProductControllerIT.java
+++ src/test/java/hu/webler/weblerstoragemanager/controller/ProductControllerIT.java
@@ -3,6 +3,7 @@ package hu.webler.weblerstoragemanager.controller;
 import hu.webler.weblerstoragemanager.value.Category;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class ProductControllerIT {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
